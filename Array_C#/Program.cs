0eca0b56296c09bcf31b87386572e88a1126ba75
sanajycad01 <sanjayrbcad01@gmail.com>
2024-02-21 14:26:45 +0530

    Modified FIle Are Upload

---
+++ Array_C#/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program{
+    public static void Main(string[] args)
+    {
+        int[] arr={2,9,6,5,4};
+        for(int i=0;i<arr.lenght;i++)
+        {
+            for(int j=i;j<arr.length;j++)
+            {
+                if(a[i]>a[j])
+                {
+                    
+                }
+
+            }
+        }
+    }
+
+
+    }
+   /*  public static void Main(string[] args)
+    {
+        int[] arr={1,2,3,4,5};
+        int a=arr.Length;
+        Console.WriteLine(a);
+        foreach(int b in arr)
+        {
+            Console.WriteLine(b);
+        }
+        for(int i=a-1;i>=0;i--)
+        {
+            Console.WriteLine(arr[i]);
+        }
+        
+    } */
+
